aac4725565daa7663f28585e2953a18bc5b10bcf
DescafeinadoBR <saulo_carvalho123@hotmail.com>
2023-08-08 14:14:23 -0300

    CoursesTableSeeder

---
+++ database/seeders/CoursesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CoursesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
