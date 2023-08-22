0a734a966f51eab4009aea2b19c331b3d184053f
MarcoSilva0 <marcoantonio02016@outlook.com>
2023-08-22 02:03:21 -0300

    feat: inserted request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
      */
     public function allUsers(Request $request)
     {
-
+        $limit = $request->input('limit');
         $Users = User::paginate($limit);
         return response()->json(['data' =>  $Users], 200);
     }
